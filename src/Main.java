ae6b58516333b3f7f7fbeb0f1066076ff98abf90
Moline-x <2223745240@qq.com>
2019-12-09 14:53:35 +0800

    二叉搜索树复习第一次提交

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        Random random = new Random();
+        int n = 1000;
+        for (int i = 0; i < n; i++) {
+            bst.add(random.nextInt(10000));
+        }
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty()){
+            nums.add(bst.removeMin());
+        }
+        System.out.println(nums);
+        for (int i = 1; i < nums.size(); i++) {
+            if(nums.get(i-1) > nums.get(i)){
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("RemoveMin test complete!");
+
+    }
+}
